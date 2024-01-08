1b0446dc5beea0652abbf64a5ce73ed030d5d155
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2024-01-08 21:25:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.mkv'], // Include .mkv files in asset imports
+  base:"/Rhine-Ho.github.io/",
 });
