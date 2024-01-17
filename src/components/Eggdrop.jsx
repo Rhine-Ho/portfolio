30d3a71c027c603064f6b59d946250bfe5276e6e
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2024-01-17 09:05:40 +0800

    reset path

--- src/components/Eggdrop.jsx
+++ src/components/Eggdrop.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import About from './about';
-import Rhine from '../assets/rhine.JPG';
+import Rhine from '../assets/Rhine.jpg';
 
 
 const Eggdrop = () => {
